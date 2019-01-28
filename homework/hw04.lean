6204a3e4eff6f56d916aa73b900b2c8c5529c7f9
williamdemeo <williamdemeo@gmail.com>
2019-01-28 11:12:19 -0700

    update hw 4

--- homework/hw04.lean
+++ homework/hw04.lean
@@ -6,29 +6,28 @@
 open classical
 variables {A B C : Prop}
 
+
 lemma step1 (h₁ : ¬ (A ∧ B)) (h₂ : A) : ¬ A ∨ ¬ B :=
-have ¬ B, from (assume b : B, have x:A ∧ B, from and.intro h₂ b, h₁ x),
+have ¬ B, from sorry,
 show ¬ A ∨ ¬ B, from or.inr this
 
 lemma step2 (h₁ : ¬ (A ∧ B)) (h₂ : ¬ (¬ A ∨ ¬ B)) : false :=
 have ¬ A, from
-  assume A,
+  assume : A,
   have ¬ A ∨ ¬ B, from step1 h₁ ‹A›,
-  show false, from h₂ ‹¬ A ∨ ¬ B›,
+  show false, from h₂ this,
 show false, from sorry
 
 theorem step3 (h : ¬ (A ∧ B)) : ¬ A ∨ ¬ B :=
 by_contradiction
   (assume h' : ¬ (¬ A ∨ ¬ B),
-  show false, from step2 h h')
-
-
+    show false, from step2 h h')
+    
 
 -- 5.3.7. This exercise is optional (not required).
 -- open classical
 -- variables {A B C : Prop}
 
--- example (h : ¬ B → ¬ A) : A → B := sorry
+example (h : ¬ B → ¬ A) : A → B := sorry
 
--- example (h : A → B) : ¬ A ∨ B := 
--- sorry
+example (h : A → B) : ¬ A ∨ B := sorry
