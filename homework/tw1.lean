1eeb5d4a41be0de21e73a66658f994df63e106fc
williamdemeo <williamdemeo@gmail.com>
2019-01-28 11:04:23 -0700

    update

--- homework/tw1.lean
+++ homework/tw1.lean
@@ -1,56 +1,28 @@
 -- Teamwork 1
 -- Due: Friday 15 February by 2pm
 
+-- Instructions. Replace each "sorry" with a proof of the given statement.
+
 variables A B C D : Prop
 
 -- Exercise 1
-example : A ∧ (A → B) → B :=
-  assume h : A ∧ (A → B),
-  have ha : A, from and.elim_left h,
-  have hab : A → B, from and.elim_right h,
-  show B, from hab ha 
-
+example : A ∧ (A → B) → B := sorry
 
 -- Exercise 2
-example : A → ¬ (¬ A ∧ B) := 
-  assume h : A, 
-  assume hc : ¬ A ∧ B,
-  show false, from (and.elim_left hc) h
+example : A → ¬ (¬ A ∧ B) := sorry
 
-example  (h : false) : A := false.elim h
+example  (h : false) : A := sorry
 
 -- Exercise 3
-example : ¬ (A ∧ B) → (A → ¬ B) :=
-  assume hnab : ¬ (A ∧ B), 
-  assume ha : A,
-  assume hb : B, 
-  show false, from
-    have hab : A ∧ B, from and.intro ha hb,
-    hnab hab
-
+example : ¬ (A ∧ B) → (A → ¬ B) := sorry
 
 -- Exercise 4
-example (h₁ : A ∨ B) (h₂ : A → C) (h₃ : B → D) : C ∨ D :=
-  or.elim h₁
-    (assume ha : A, or.inl (h₂ ha))
-    (assume hb : B, or.inr (h₃ hb) )
-
+example (h₁ : A ∨ B) (h₂ : A → C) (h₃ : B → D) : C ∨ D := sorry
 
 -- Exercise 5
-example (h : ¬ A ∧ ¬ B) : ¬ (A ∨ B) :=
-  assume h1 : A ∨ B, show false, from 
-  or.elim h1
-    (assume ha : A, (and.elim_left h) ha)
-    (assume hb : B, (and.elim_right h) hb)
+example (h : ¬ A ∧ ¬ B) : ¬ (A ∨ B) := sorry
 
 -- Exercise 6
-example : ¬ (A ↔ ¬ A) := 
-  assume h : A ↔ ¬ A, show false, from 
-  have hna : ¬ A, from 
-    (assume ha : A, show false, from
-      have h' : ¬ A, from (iff.elim_left h) ha,
-      h' ha),
-  have ha : A, from (iff.elim_right h) hna,
-  hna ha
+example : ¬ (A ↔ ¬ A) := sorry
 
 
